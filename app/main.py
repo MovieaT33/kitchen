fb0e8d6ca47255cef49f4a874460f189b623c2db
Nikolas <132286086+MovieaT33@users.noreply.github.com>
2024-05-28 12:12:53 +0000

    modified:   .gitignore new file:   app/config/.env new file:   app/db/1.0.1_foods.pkl new file:   app/db/1.2_foods.pkl new file:   app/db/test_foods.pkl new file:   "app/db/\321\201\320\273\320\260\320\264\320\265\320\275\321\214\320\272\320\276\320\265_foods.pkl" modified:   app/main.py modified:   app/models/database.py

--- app/main.py
+++ app/main.py
@@ -1,48 +1,56 @@
 from fastapi import FastAPI
 from config import TITLE, VERSION
 from models.database import Foods, Food
+from fastapi.responses import ORJSONResponse
 
 app = FastAPI(
     title=TITLE,
     version=VERSION
 )
 
-foods: Foods = Foods([
-    Food(id=0, name="Apple"),
-    Food(id=1, name="Banana")
-])
+foods: Foods = Foods([])
 
 
-@app.post("/food")
-async def add_food(food: Food) -> dict:
-    foods.foods.append(food)
-    return {"status": 200, "foods": foods.foods}
+@app.post("/foods")
+async def add_food(new_food: Food) -> dict:
+    foods.foods.append(new_food)
+    return {"status": 201, "data": foods.foods}
 
 
 @app.get("/foods")
-async def get_foods() -> list:
-    return foods.foods
+async def get_foods() -> dict:
+    return {"status": 200, "data": foods.foods}
 
 
 @app.get("/foods/{food_id}")
-async def get_food(food_id: int) -> list:
-    return [food for food in foods.foods if food.id == food_id]
+async def get_food(food_id: int) -> dict:
+    return {"status": 200,
+            "data": [food for food in foods.foods if food.id == food_id]}
 
 
-@app.delete("/food")
+@app.patch("/foods/{food_id}")
+async def update_food(food_id: int, new_food: Food) -> dict:
+    for i, food in enumerate(foods.foods):
+        if food.id == food_id:
+            foods.foods[i] = new_food
+            return {"status": 200, "data": foods.foods}
+    return {"status": 404}
+
+
+@app.delete("/foods/{food_id}")
 async def delete_food(food_id: int) -> dict:
     for food in foods.foods:
         if food.id == food_id:
             foods.foods.remove(food)
-            return {"status": 200, "foods": foods.foods}
+            return {"status": 200, "data": foods.foods}
     return {"status": 404}
 
 
-@app.post("/save")
-async def save(version: str) -> dict:
+@app.post("/foods/save", response_class=ORJSONResponse)
+async def save(version: str) -> ORJSONResponse:
     return foods.save(version)
 
 
-@app.post("/load")
-async def load(version: str) -> dict:
+@app.post("/foods/load", response_class=ORJSONResponse)
+async def load(version: str) -> ORJSONResponse:
     return foods.load(version)
--- app/models/database.py
+++ app/models/database.py
@@ -1,30 +1,37 @@
 import pickle
 from pydantic import BaseModel
+from fastapi.responses import ORJSONResponse
+import os
 
 
 class Food(BaseModel):
-    id: int
-    name: str
+    id:          int
+    name:        str
+    description: str | None
 
 
 class Foods:
     def __init__(self, foods: list[Food]) -> None:
         self.foods = foods
 
-    def save(self, version: str) -> dict:
+    def save(self, version: str) -> ORJSONResponse:
         try:
-            with open(f"db/{version}_foods.pkl", "wb") as file:
+            with open(
+                f"db/{os.path.basename(version)}_foods.pkl", "wb"
+                    ) as file:
                 pickle.dump(self.foods, file)
 
-            return {"status": 200}
+            return ORJSONResponse({"status": 200, "filename": file.name})
         except OSError:
-            return {"status": 405}
+            return ORJSONResponse({"status": 405})
 
-    def load(self, version: str) -> dict:
+    def load(self, version: str) -> ORJSONResponse:
         try:
-            with open(f"db/{version}_foods.pkl", "rb") as file:
+            with open(
+                f"db/{os.path.basename(version)}_foods.pkl", "rb"
+                    ) as file:
                 self.foods = pickle.load(file)
 
-            return {"status": 200}
+            return ORJSONResponse({"status": 200, "filename": file.name})
         except OSError:
-            return {"status": 405}
+            return ORJSONResponse({"status": 405})
